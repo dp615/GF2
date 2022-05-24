b168135e7b969c34ee22e9e8acc4950e3fc3fedf
KusalF <wkvf2@cam.ac.uk>
2022-05-24 22:20:21 +0100

    Parse_devices body done, error handling tbc

--- logsim/parse.py
+++ logsim/parse.py
@@ -44,11 +44,92 @@ class Parser:
         self.current_symbol = None
         self.successful_parse = True
 
+
     def next_symbol(self):
         self.current_symbol = self.scanner.get_symbol()
 
-    def parse_devices(self):
+    def next_scan_start(self):
+        while not (
+                self.current_symbol.type == self.scanner.SEMICOLON 
+                or self.current_symbol.type == self.scanner.EOF
+                or (self.current_symbol.type == self.scanner.KEYWORD
+                and self.current_symbol.id == self.scanner.END_ID)
+              ):
+            self.next_symbol()
+
+    def add_device(self):
         pass
+
+    def add_connection(self):
+        pass
+
+    def add_monitor(self):
+        pass
+
+    def valid_parameter(self,device_type_id,parameter):
+        return True
+
+    def valid_device_name(self,device_name_id):
+        return True
+
+    def parse_devices(self):
+        while True:
+            self.next_symbol()
+            if (
+                    self.current_symbol.type == self.scanner.NAME 
+                    and (self.current_symbol.id in self.devices.device_types 
+                    or self.current_symbol.id in self.devices.gate_types)
+                ):
+                
+                device_type_id = int(self.current_symbol.id)
+                parameter = None
+
+                self.next_symbol()
+                expect_equals = True
+                if self.current_symbol.type == self.scanner.COMMA:
+                    expect_equals = False
+                    self.next_symbol()
+                    if self.current_symbol.type == self.scanner.NUMBER:
+                        parameter = int(self.current_symbol.id)
+                        if not self.valid_parameter(device_type_id,parameter):
+                            self.successful_parse = False
+                            # SEMANTIC ERROR (Invalid parameter for device)
+                        else:
+                            self.next_symbol()
+                            expect_equals = True
+                    else:
+                        self.successful_parse = False
+                        #SYNTAX ERROR (Expected a number here)
+
+                if self.current_symbol.type == self.scanner.EQUALS and expect_equals == True:
+                    self.next_symbol()
+                    if self.current_symbol.type == self.scanner.NAMES:
+                        device_name_id = int(self.current_symbol.id)
+                        if not self.valid_device_name_id(device_name_id):
+                            self.successful_parse = False
+                            # SEMANTIC ERROR (Invalid device name (same as existing device name or device type))
+                        else:
+                            self.next_symbol()
+                            if self.current_symbol.type == self.scanner.SEMICOLON:
+                                self.add_device()
+                                #Need to add ability to add a device here
+                            else:
+                                self.successful_parse = False
+                                #SUNTAX ERROR MESSAGE (Expected a semicolon)
+                    else:
+                        self.successful_parse = False
+                        #SYNTAX ERROR MESSAGE (Expected an alphanumeric string for device name)
+                else:
+                    self.successful_parse = False
+                    #SYNTAX ERROR MESSAGE (Expected equals sign here)     
+                               
+            elif self.current_symbol.type == self.scanner.KEYWORD and self.current_symbol.id == self.scanner.END_ID:
+                return
+            elif self.current_symbol.type == self.scanner.EOF:
+                return
+            else:
+                self.successful_parse = False
+                #SYNTAX ERROR MESSAGE (Unrecognised Device Type)
 
     def parse_connections(self):
         pass
@@ -71,13 +152,13 @@ class Parser:
                         self.next_symbol()
                     else:
                         self.successful_parse = False
-                        #SYNTAX ERROR MESSAGE
+                        #SYNTAX ERROR MESSAGE (NO MONITORS)
                 else:
                     self.successful_parse = False
-                    #SYNTAX ERROR MESSAGE
+                    #SYNTAX ERROR MESSAGE (NO CONNECTIONS)
             else:
                 self.successful_parse = False
-                #SYNTAX ERROR MESSAGE
+                #SYNTAX ERROR MESSAGE (NO DEVICES)
             return self.successful_parse
 
         # For now just return True, so that userint and gui can run in the
