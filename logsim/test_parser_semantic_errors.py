31d96683926294dfc005effbe7941e1f5a362a8e
David Pattison <dp615@cam.ac.uk>
2022-05-31 15:35:19 +0100

    PEP8 test_parse_semantic_errors

--- logsim/test_parser_semantic_errors.py
+++ logsim/test_parser_semantic_errors.py
@@ -9,19 +9,20 @@ from monitors import Monitors
 
 
 def test_parser_semantic_errors_devices(capsys):
-    errors = ['Device by this name already exists',
-              'Qualifier is invalid for device type',
-              'No qualifier given and device type requires one',
-              'Qualifier given but one was not allowed with device type'
-              , 'Device Type given is not a valid device type']
+    errors = [
+        "Device by this name already exists",
+        "Qualifier is invalid for device type",
+        "No qualifier given and device type requires one",
+        "Qualifier given but one was not allowed with device type",
+        "Device Type given is not a valid device type",
+    ]
     for i in range(5):
         names = Names()
         devices = Devices(names)
         network = Network(names, devices)
         monitors = Monitors(names, devices, network)
 
-        file_path = r'parser_semantic_error_tests/' + str(i + 1) \
-            + '.txt'
+        file_path = r"parser_semantic_error_tests/" + str(i + 1) + ".txt"
         scanner = Scanner(file_path, names)
         parser = Parser(names, devices, network, monitors, scanner)
         parser.parse_network()
@@ -32,13 +33,13 @@ def test_parser_semantic_errors_devices(capsys):
 
 def test_parser_semantic_errors_connections(capsys):
     errors = [
-        'ERROR : Device name does not exist',
-        'ERROR : Device name does not exist',
-        'ERROR : Input is already connected',
-        'ERROR : Cannot connect an input to an input',
-        'ERROR : Port does not exist',
-        'ERROR : Cannot Connect output to output',
-        ]
+        "ERROR : Device name does not exist",
+        "ERROR : Device name does not exist",
+        "ERROR : Input is already connected",
+        "ERROR : Cannot connect an input to an input",
+        "ERROR : Port does not exist",
+        "ERROR : Cannot Connect output to output",
+    ]
 
     for i in range(6):
         names = Names()
@@ -46,8 +47,7 @@ def test_parser_semantic_errors_connections(capsys):
         network = Network(names, devices)
         monitors = Monitors(names, devices, network)
 
-        file_path = r'parser_semantic_error_tests/' + str(i + 6) \
-            + '.txt'
+        file_path = r"parser_semantic_error_tests/" + str(i + 6) + ".txt"
         scanner = Scanner(file_path, names)
         parser = Parser(names, devices, network, monitors, scanner)
         parser.parse_network()
@@ -57,9 +57,11 @@ def test_parser_semantic_errors_connections(capsys):
 
 
 def test_parser_semantic_errors_monitor(capsys):
-    errors = ['ERROR : Can only monitor outputs',
-              'ERROR : Output already being monitored',
-              'ERROR : Device does not exist']
+    errors = [
+        "ERROR : Can only monitor outputs",
+        "ERROR : Output already being monitored",
+        "ERROR : Device does not exist",
+    ]
 
     for i in range(3):
         names = Names()
@@ -67,13 +69,10 @@ def test_parser_semantic_errors_monitor(capsys):
         network = Network(names, devices)
         monitors = Monitors(names, devices, network)
 
-        file_path = r'parser_semantic_error_tests/' + str(i + 12) \
-            + '.txt'
+        file_path = r"parser_semantic_error_tests/" + str(i + 12) + ".txt"
         scanner = Scanner(file_path, names)
         parser = Parser(names, devices, network, monitors, scanner)
         parser.parse_network()
     (out, err) = capsys.readouterr()
     for i in range(3):
         assert errors[i] in out
-
-
