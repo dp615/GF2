1affa346149fdbabc33fc3211f24b5a0fba30205
Ollie Parish <ollie.parish@gmail.com>
2022-06-01 12:42:22 +0100

    clean up cnf display, fixed errors in graph.py, added lots of gui test files

---
+++ logsim/test_gui.py
@@ -0,0 +1,15 @@
+"""Test the Gui and MyGlCanvas modules."""
+import pytest
+
+import wx
+import wx.glcanvas as wxcanvas
+from OpenGL import GL, GLUT
+
+from names import Names
+from devices import Devices
+from network import Network
+from monitors import Monitors
+from scanner import Scanner
+from parse import Parser
+from graph import Graph
+
