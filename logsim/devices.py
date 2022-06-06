fb503563b2e8135faa178590a3167ed9dec4b83c
KusalF <wkvf2@cam.ac.uk>
2022-06-06 09:22:46 +0100

    xor gate fixed

--- logsim/devices.py
+++ logsim/devices.py
@@ -321,7 +321,7 @@ class Devices:
                 else:
                     self.make_gate(device_id, device_kind, 2)
                     error_type = self.NO_ERROR
-            if device_kind == self.NOT:
+            elif device_kind == self.NOT:
                 if device_property is not None:
                     error_type = self.QUALIFIER_PRESENT
                 else:
