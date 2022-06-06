f3d0131985f7e48e0dd2d0040e5b3f0662c05f0b
Ollie Parish <ollie.parish@gmail.com>
2022-06-06 13:54:48 +0100

    pep8 stuff

--- logsim/gui.py
+++ logsim/gui.py
@@ -111,7 +111,9 @@ class MyGLCanvas(wxcanvas.GLCanvas):
     def init_gl(self):
         """Configure and initialise the OpenGL context."""
         size = self.GetClientSize()
-        if self.IsShownOnScreen():
+
+        # Include "not" below for windows but leave it out for linux
+        if not self.IsShownOnScreen():
             self.SetCurrent(self.context)
         GL.glDrawBuffer(GL.GL_BACK)
         GL.glViewport(0, 0, size.width, size.height)
@@ -355,8 +357,8 @@ class MyGLCanvas(wxcanvas.GLCanvas):
             self.canvas_size[1] - 50)
         bool_exp = self.parent.graph.create_boolean_from_monitor(mon_name)
         if not bool_exp:
-            self.render_text(_('Flip-Flop or circular definition in graph, try '
-                             'a different logic circuit'), 10,
+            self.render_text(_('Flip-Flop or circular definition in graph, try'
+                             ' a different logic circuit'), 10,
                              self.canvas_size[1] - 50 - line_gap)
             GL.glFlush()
             self.SwapBuffers()
@@ -377,8 +379,7 @@ class MyGLCanvas(wxcanvas.GLCanvas):
         bool_exp_show, new_extra_lines = \
             self.parent.graph.add_new_line_breaks(bool_exp2)
         self.render_text(bool_exp_show + '\n \t\t(expand XORs to ANDs/ORs)',
-                         10,
-                         self.canvas_size[1] - 30 - line_gap * 2 -
+                         10, self.canvas_size[1] - 30 - line_gap * 2 -
                          extra_lines * 20)
         extra_lines += new_extra_lines
 
@@ -560,7 +561,7 @@ class Gui(wx.Frame):
         toolbar.AddTool(self.logic_id, _("Logic Description"), myimage)
         myimage = wx.ArtProvider.GetBitmap(wx.ART_EXECUTABLE_FILE,
                                            wx.ART_TOOLBAR)
-        toolbar.AddTool(self.cnf_id,_("CNF"), myimage)
+        toolbar.AddTool(self.cnf_id, _("CNF"), myimage)
         myimage = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR)
         toolbar.AddTool(self.open_id, _("Open file"), myimage)
         myimage = wx.ArtProvider.GetBitmap(wx.ART_HELP, wx.ART_TOOLBAR)
@@ -578,7 +579,8 @@ class Gui(wx.Frame):
 
         self.continue_button = wx.Button(self, wx.ID_ANY, _("Continue"))
 
-        self.text_switch_control = wx.StaticText(self, wx.ID_ANY, _("Switch On:"))
+        self.text_switch_control = wx.StaticText(self, wx.ID_ANY,
+                                                 _("Switch On:"))
         self.switch_choice = wx.ComboBox(self, wx.ID_ANY, "<SWITCH>",
                                          choices=self.switch_names)
         self.switch_choice.SetValue(self.switch_names[0])
@@ -591,7 +593,8 @@ class Gui(wx.Frame):
         self.remove_monitor_button = wx.Button(self, wx.ID_ANY, _("Remove"))
         self.add_monitor_choice = wx.ComboBox(self, wx.ID_ANY, _("<SIGNAL>"),
                                               choices=self.sig_n_mons)
-        self.remove_monitor_choice = wx.ComboBox(self, wx.ID_ANY, _("<SIGNAL>"),
+        self.remove_monitor_choice = wx.ComboBox(self, wx.ID_ANY,
+                                                 _("<SIGNAL>"),
                                                  choices=self.sig_mons)
         if len(self.sig_n_mons):
             self.add_monitor_choice.SetValue(self.sig_n_mons[0])
@@ -704,7 +707,7 @@ class Gui(wx.Frame):
             openFileDialog = wx.FileDialog(self, _("Open txt file"), "", "",
                                            wildcard="TXT files (*.txt)|*.txt",
                                            style=wx.FD_OPEN +
-                                                 wx.FD_FILE_MUST_EXIST)
+                                           wx.FD_FILE_MUST_EXIST)
             self.reset_screen()
             if openFileDialog.ShowModal() == wx.ID_CANCEL:
                 print(_("The user cancelled"))
@@ -910,7 +913,6 @@ class Gui(wx.Frame):
 
     def on_remove_connection_button(self, event):
         """Handle the event when the user wants to remove a connection."""
-
         con_name = self.remove_connection_choice.GetValue()
 
         con_id = self.con_ids[self.con_names.index(con_name)][1]
